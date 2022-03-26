6976ad48cd6db3e78ec0475b6c724a2718e74139
Yeferson <yadauto@soaint.com>
2022-03-26 12:03:11 -0500

    Se quito la linea de codigo

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,8 +4,6 @@ public class PersonaRegistro {
 
   @Override
   public String toString() {
-    // Yeferson: comente la linea de codigo que considero se puede eliminiar
-    System.out.println("Esta instruccion es inservible");
     return super.toString();
   }
 }
